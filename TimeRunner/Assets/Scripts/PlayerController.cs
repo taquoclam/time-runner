f50472d7c94e545101b2030f1adc45e5bacfda86
Seong Kim <hoon5421@hotmail.com>
2017-09-27 15:38:01 -0400

    Adding base of our game
    Player can only jump on the ground by pressing space.

---
+++ TimeRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpSpeed;
+	private Rigidbody2D myRigidbody;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;
+	public bool isGrounded;
+	private Animator myAnim;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+		jumpSpeed = 12;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, groundLayer);
+
+		if (Input.GetButtonDown ("Jump") && isGrounded) {
+			myRigidbody.velocity = new Vector3 (myRigidbody.velocity.x, jumpSpeed, 0f);
+		}
+
+		myAnim.SetFloat ("Speed", Mathf.Abs (myRigidbody.velocity.x));
+		myAnim.SetBool ("Grounded", isGrounded);
+
+	}
+}
