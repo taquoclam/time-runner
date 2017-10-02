9773654e7ddd68f5b212b10e2da9ab729fb143eb
Serguei Khasimkhanov <serkhas@hotmail.com>
2017-10-02 14:02:04 -0400

    Make background teleportation less noticeable

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -6,31 +6,24 @@ public class RepeatBackground : MonoBehaviour {
 
 	private BoxCollider2D groundCollider;
 	private float groundHorizontalLength;
-	private float orderLayer;
+    private float startx;
 
 	// Use this for initialization
 	void Start () {
 		groundCollider = GetComponent<BoxCollider2D> ();
 		groundHorizontalLength = groundCollider.size.x;
-
-		//This is to prevent objects from blocking player
-		if (gameObject.tag == "ground") {
-			orderLayer = 1;
-		} else {
-			orderLayer = 2;
-		}
+        startx = transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.x < -groundHorizontalLength) {
+		if (transform.position.x + groundHorizontalLength < startx) {
 			RepositionBackground ();
 		}
 	}
 
 	private void RepositionBackground(){
-		Vector3 groundOffset = new Vector3 (groundHorizontalLength * 2f, 0, orderLayer);
-		transform.position = (Vector3)transform.position + groundOffset;
+        transform.position += new Vector3(startx - transform.position.x, 0, 0);
 	}
 
 }
