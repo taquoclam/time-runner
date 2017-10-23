354c5fc1c0a2328f4107abb7cbcd0d511df315b2
Serguei Khasimkhanov <serkhas@gmail.com>
2017-10-23 19:33:58 -0400

    Add layers, half volume

--- Assets/Scripts/Enemy/Enemies Projectile & Weapons/EnemiesRockPro.cs
+++ Assets/Scripts/Enemy/Enemies Projectile & Weapons/EnemiesRockPro.cs
@@ -8,7 +8,6 @@ namespace Projectiles
         {
             rb = GetComponent<Rigidbody2D>();
             rb.velocity = PlayerController.DirectionFrom(transform.position) * 5.0f - new Vector2(5.0f, 0);
-            Debug.LogFormat("Speed is {0} but should be 8*{1}", rb.velocity, PlayerController.DirectionFrom(transform.position));
         }
         private void OnTriggerEnter2D(Collider2D coll)
         {
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public float scrollSpeed = -1.5f;
 
+
     void Awake()
     {
+        // Instantiation
         if (instance == null)
         {
             instance = this;
@@ -14,6 +18,33 @@ public class GameControl : MonoBehaviour
         else if (instance != this)
         {
             Destroy(gameObject);
+        }
+        
+        // Layer interaction
+        Physics2D.IgnoreLayerCollision(Layers.Enemies, Layers.Enemies); // so enemies don't bump into each other
+        Physics2D.IgnoreLayerCollision(Layers.Player, Layers.Player); // player & projectile
+        Physics2D.IgnoreLayerCollision(Layers.Player, Layers.Enemies, false); // players and enemies collide
+        
+        // Half the volume since current sound effects are p loud
+        AudioListener.volume = 0.5f;
+    }
+
+    // layer numbers
+    public static class Layers
+    {
+        public static readonly int
+            Background = LayerMask.NameToLayer("Background"),
+            Ground = LayerMask.NameToLayer("Ground"),
+            Player = LayerMask.NameToLayer("Player"),
+            Enemies = LayerMask.NameToLayer("Enemies");
+
+        static Layers()
+        {
+            // sanity check
+            Assert.Greater(Background, -1);
+            Assert.Greater(Ground, -1);
+            Assert.Greater(Player, -1);
+            Assert.Greater(Enemies, -1);
         }
     }
 
